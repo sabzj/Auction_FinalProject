298be21ca2675e2da3ec820b481068b2d8d62ddb
sabzj <zjsabbah@gmail.com>
2024-01-17 16:08:59 +0200

    change name file

--- server/routes/cloudinary.routes.js
+++ server/routes/cloudinary.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import upload from "../middleware/multer.js";
-import { uploadImage } from "../controllers/cloudinaryRoute.js";
+import { uploadImage } from "../controllers/cloudinaryController.js";
 const router = Router();
 
 router.post("/upload", upload.single("image"), uploadImage);
